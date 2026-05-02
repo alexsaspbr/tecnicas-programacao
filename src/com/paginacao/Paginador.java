6ded875cfc5a592a99fa7a2494908babce88764e
Lucas Yago <lucasyago63@gmail.com>
2026-05-02 17:13:05 -0300

    feat(pagination): implementa exercício 4

--- src/com/paginacao/Paginador.java
+++ src/com/paginacao/Paginador.java
@@ -69,6 +69,6 @@ public class Paginador<T> {
      */
     public int totalPaginas(List<T> lista, int tamanhoPagina) {
         // TODO: implemente
-        throw new UnsupportedOperationException("Não implementado");
+        return( lista.size() + tamanhoPagina -1 ) / tamanhoPagina;
     }
 }
