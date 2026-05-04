2d78e151f9538df642cf80a380e2afc240756fc0
devlucao <devlucao@gmail.com>
2026-05-04 14:55:32 -0300

    [feat] Resolve exercício 3 - Ignora os primeiros N itens da lista

--- src/com/paginacao/TestePaginador.java
+++ src/com/paginacao/TestePaginador.java
@@ -3,20 +3,33 @@ package com.paginacao;
 import java.util.Arrays;
 import java.util.List;
 
+// Testes isolados dos exercícios
 public class TestePaginador {
   public static void main (String[] args) {
     List<String> nomes = Arrays.asList("A", "B", "C", "D", "E");
     Paginador<String> paginador = new Paginador<>();
 
+    // Testando Exercício
     System.out.println("Testando o Exercício 2 - PrimeirosN");
-    List<String> resultado = paginador.primeirosN(nomes, 3);
+    List<String> resultado2 = paginador.primeirosN(nomes, 3);
 
     System.out.println("Esperado: [A, B, C]");
-    System.out.println("Obtido: " + resultado);
+    System.out.println("Obtido: " + resultado2);
 
     // Teste de borda - pedir mais do que existe
-    List<String> resultadoMaior = paginador.primeirosN(nomes, 10);
-    System.out.println("Teste: n = 10 - Maior que a lista: " + resultadoMaior);
+    List<String> resultadoMaior2 = paginador.primeirosN(nomes, 10);
+    System.out.println("Teste: n = 10 - Maior que a lista: " + resultadoMaior2);
 
+    System.out.println("Testando o Exercício 3 - IgnorarN");
+    List<String> resultado3 = paginador.ignorarN(nomes, 2);
+
+    System.out.println("Esperado: [C, D, E]");
+    System.out.println("Obtido: " + resultado3);
+
+    List<String> resultadoMaior3 = paginador.ignorarN(nomes, 10);
+    System.out.println("Teste: n = 10 - Maior que a lista: " + resultadoMaior3);
+
+    System.out.println("Esperado: []");
+    System.out.println("Obtido:   " + resultadoMaior3);
   }
 }
