b831fe381136f9547eb6965b1f37d45fe1d5b5f6
Alex Silva Araujo <alexsaspbr@gmail.com>
2026-05-08 21:47:29 -0300

    feat(io_streams): exemplo exercicio 3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class Main {
 
 
     public static void main(String[] args) throws IOException {
 
-        byte[] bytes       = Files.readAllBytes(Path.of(""));
-        var bytes2         = Files.readAllBytes(Path.of(""));
+       // byte[] bytes       = Files.readAllBytes(Path.of(""));
+       // var bytes2         = Files.readAllBytes(Path.of(""));
+
+        String alunoNota = "alex,2.0";
+        String[] arrayAlunoNota = alunoNota.split(",");
+
+        System.out.println(arrayAlunoNota[0]);
+        System.out.println(Double.parseDouble(arrayAlunoNota[1]));
 
         try {
             copy();
